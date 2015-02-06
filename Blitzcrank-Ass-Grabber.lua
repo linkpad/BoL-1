b8fbabdad9d9b937b3fb4a61b07447792e3cfa93
AMBER17 <amberhacking17@gmail.com>
2015-02-06 19:10:23 +0100

    Update Blitzcrank-Ass-Grabber.lua

--- Blitzcrank-Ass-Grabber.lua
+++ Blitzcrank-Ass-Grabber.lua
@@ -1,6 +1,6 @@
 if not VIP_USER or myHero.charName ~= "Blitzcrank" then return end
 
-local  BlitzcrankAssGrabber_Version = 1.8
+local  BlitzcrankAssGrabber_Version = 1.9
 
 class "SxUpdate"
 function SxUpdate:__init(LocalVersion, Host, VersionPath, ScriptPath, SavePath, Callback)
